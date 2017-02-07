60b246ff0296bd442c548d049bf7081d7ef92690
devbharat <bharat.tak8@gmail.com>
2017-02-07 18:39:48 +0100

    This is so good. Now have direct variable access with names, and tab prompts on ipython

--- deh5py.py
+++ deh5py.py
@@ -5,16 +5,15 @@ import h5py
 
 M = h5py.File('company_data.hdf5')
 
-"""
 for label in M.keys():
-	exec("%s = %s" % (label, M[label][:]))
+	exec('%s = M["%s"][:]' % (label, label))
 
 plt.figure(1)
 plt.plot(TIME_StartTime, ACTR_fSat0)
 plt.hold('on')
 plt.plot(TIME_StartTime, ACTR_fSat1)
 plt.plot(TIME_StartTime, -np.array(CTD1_eR2)*np.array(CTD2_kR2))
-plt.plot(TIME_StartTime, -np.array(CTD1_eq)*np.array(CTD2_kRate2))
+plt.plot(TIME_StartTime, -CTD1_eq*CTD2_kRate2)
 plt.plot(TIME_StartTime,FOMO_myInt, 'x')
 plt.plot(TIME_StartTime,CTD5_MyFF)
 plt.plot(TIME_StartTime,FOMO_my)
@@ -34,3 +33,4 @@ plt.plot(M["TIME_StartTime"][:],M["CTD5_MyFF"][:])
 plt.plot(M["TIME_StartTime"][:],M["FOMO_my"][:])
 plt.grid('on')
 plt.show()
+"""
